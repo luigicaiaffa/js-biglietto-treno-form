d788bd7ce9f8fc30445d46f19e62e0750429f09a
luigicaiaffa <luigicaiaffa8@gmail.com>
2024-10-23 15:23:20 +0200

    raccolta dati

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// # RACCOLTA DATI
+
+// prendo input nome cognome passeggero
+const fullNameInput = document.getElementById("input-fullname");
+// prendo input km da percorrere
+const kmInput = document.getElementById("input-km");
+
+// assegno il prezzo del biglietto per km
+const ticketPriceKm = 0.21;
+
+// # ELABORAZIONE
